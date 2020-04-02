cff9fd951fba292334f176e25921abc250eb6cee
conejitomaya <eduardobonillac@gmail.com>
2020-04-02 11:40:52 -0600

    presence and lenght validations added to models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :amount, presence: true
 end
