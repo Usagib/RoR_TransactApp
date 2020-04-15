751c754ac6b0e451a41e7a72d2dc36cb4d790b06
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-04-15 17:48:31 -0500

    routes re-configured, deleted custom routes
    Sergio Torres comply 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/new_group', to: 'groups#new'
-  post '/new_group', to: 'groups#create'
-  get '/groups', to: 'groups#index'
+  root 'mytransactions#index'
   get '/external', to: 'mytransactions#external'
 
-  get '/new_mytransaction', to: 'mytransactions#new'
-  post '/new_mytransaction', to: 'mytransactions#create'
-  get '/mytransactions', to: 'mytransactions#index'
-
-  root 'mytransactions#index'
-
+  post '/groups/new', to: 'groups#create'
+  post '/mytransactions/new', to: 'mytransactions#create'
   post '/users/new', to: 'users#create'
 
   get    '/login',   to: 'sessions#new'
