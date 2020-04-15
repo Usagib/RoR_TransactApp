c0b620402d736dd93845b56571ffb0e0a14ed7d8
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-04-15 17:17:59 -0500

    Deleted group? ungroup? methods, added scopes and null options
    Bola Ahmed review comply 2

--- app/helpers/mytransaction_helper.rb
+++ app/helpers/mytransaction_helper.rb
@@ -1,16 +1,16 @@
 module MytransactionHelper
   def total_group_amount
     total = 0
-    @current_user.mytransactions.each do |t|
-      total += t.amount if t.group?
+    @current_user.mytransactions.grouped.each do |t|
+      total += t.amount
     end
     total
   end
 
   def total_ungrouped_amount
     total = 0
-    @current_user.mytransactions.each do |t|
-      total += t.amount unless t.group?
+    @current_user.mytransactions.ungrouped.each do |t|
+      total += t.amount
     end
     total
   end
