e1c2ea29fff6b2113dcdf87d2e7a15868fe0f546
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-04-16 12:33:29 -0500

    ordered transactions trough scope
    Sergio Torres Comply 2

--- app/models/mytransaction.rb
+++ app/models/mytransaction.rb
@@ -5,6 +5,7 @@ class Mytransaction < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :amount, presence: true
 
+  default_scope -> { order(created_at: :desc) }
   scope :grouped, -> { where.not("group_id IS NULL") }
   scope :ungrouped, -> { where("group_id IS NULL") }
 
